ae8277f2facbc1a83c88bf4a57ea90f899fb9365
Salim Batlouni <batlounis@gmail.com>
2012-12-08 09:26:13 +0000

    intro page + better scoring code

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -159,19 +159,9 @@ $(document).ready(function(){
 	
 	
 	// --- End Screen Functionality ---
-	$('#view_gallery').click(function(){
-		$('.drawings').toggleClass('show');
-	});
 	
-	$('#view_score').click(function(){
-		current_screen.find('.score_page').toggleClass('show');
-	});
-	
-	$('#save_score').click(function(){
+	getScores = function(){
 		var journey_character = $('body').attr('class');
-		var score = viewModel.score();
-		var name = 'Salim';
-		
 		var stored_scores = localStorage[journey_character];
 		var scores;
 		
@@ -181,13 +171,44 @@ $(document).ready(function(){
 			scores = [];
 		}
 		
-		scores.unshift({'score':score, 'name':name});
-		localStorage[journey_character] = JSON.stringify(scores);
+		return scores;
+	}
+	
+	var score_saved = false; // Used to allow just one save per play
+	
+	addScore = function(name, score){
+		var journey_character = $('body').attr('class');
+		var scores = getScores();
+		if(!score_saved){
+			scores.unshift({'score':score, 'name':name});
+			localStorage[journey_character] = JSON.stringify(scores);
+			score_saved = true;
+		}		
+		return scores;		
+	}
+	
+	
+	$('#view_gallery').click(function(){
+		$('.drawings').toggleClass('show');
+	});
+	
+	$('#view_score').click(function(){
+		var scores = getScores();
+		viewModel.scores(scores);
+		$('.score_page').toggleClass('show');
+	});
+	
+	$('#save_score').click(function(){
+		
+		var score = viewModel.score();
+		var name = 'salim';
+		var scores = addScore(name, score);
 		
 		viewModel.scores(scores);
 				
 		$('#view_score').trigger('click');
 	});
+	
 	
 	
 	// --- Gallery browsing ---
@@ -239,7 +260,7 @@ $(document).ready(function(){
 	
 		this.scores = ko.observableArray([]);
 	
-			this.score = ko.observable(0);
+			this.score = ko.observable(13);
 			
 
 	     this.trails = ko.observableArray([]);
